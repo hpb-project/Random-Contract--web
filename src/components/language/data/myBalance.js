1d025a11866a0ab78b7fd9b9185fe7813b140ae1
Sagar <sean@gmail.com>
2022-08-26 15:54:49 +0800

    update content

--- src/components/language/data/myBalance.js
+++ src/components/language/data/myBalance.js
@@ -5,7 +5,7 @@ const myBalance={
         curBalance:"账户余额(HRG)",
         address:"账户地址",   
         submitButton: "提交", 
-        desc:"每个ip一天只能领取一次，每个地址一天只能领取一次，每次领取数量200HRG。",
+        desc:"每个ip一天只能领取一次，每个地址一天只能领取一次，每次领取数量10HRG。",
         desc2:"领取成功后需要一定得时间转到您得账户，请耐心等待。"
       
        
@@ -16,7 +16,7 @@ const myBalance={
         curBalance:"Current Balance(HRG)",
         address:"Account Address",   
         submitButton: "Submit",     
-        desc:"1 time/every account, 1 time/every IP, 200 HRG per time. ",
+        desc:"1 time/every account, 1 time/every IP, 10 HRG per time. ",
         desc2:"It will arrive at the account in a certain time, please wait. "
         
     }
